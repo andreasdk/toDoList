2648fcfde0306deed7318590969efdc12a6b00ac
Andrew Kennedy <andrew.kennedy27@outlook.com>
2018-01-06 18:16:26 +0100

    added delete function, refactored loops into functions

--- toDoList.js
+++ toDoList.js
@@ -1,17 +1,44 @@
-var todos = ["Buy New Turtle"];
+setTimeout(function(){
+
+var todos = [];
 
 var input = prompt("What would you like to do?");
 
 while(input !== "quit") {
   if(input === "list") {
-	console.log(todos);
+  	listTodos();
   } else if(input === "new") {
-	//ask for new todo
-    var newTodo = prompt("Enter new To-do");
-	//add to todos array
-    todos.push(newTodo);
+	addTodo();
+} else if(input === "delete") {
+	deleteTodo();
 }
   //ask again for new input
   input = prompt("What would you like to do?");
 }
 console.log("OK, you quit the app");
+
+function listTodos() {
+	console.log("**********")
+  	todos.forEach(function(todo, index){
+   	console.log(index + ": " + todo);
+  	});
+  	console.log("**********")
+}
+
+function addTodo() {
+	//ask for new todo
+    var newTodo = prompt("Enter new To-do");
+	//add to todos array
+    todos.push(newTodo);
+    console.log("Task added to list");
+}
+
+function deleteTodo() {
+	//asks for index of item to be deleted
+	var index = prompt("Enter index of item to delete");
+	//deletes that item
+	//splice()
+	todos.splice(index,1);
+	console.log("Task deleted");
+}
+}, 250);
